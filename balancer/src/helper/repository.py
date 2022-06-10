bcd0c953ab21677f148d9db8d529d2b6a9a341d4
Mamedov Murad <mamedov_99b@mail.ru>
2022-06-10 16:58:18 +0300

    [ADD] ADDRESSES REPOSITORY

---
+++ balancer/src/helper/repository.py
@@ -0,0 +1,32 @@
+from asyncio import Lock
+from typing import Tuple
+from datetime import datetime, timedelta
+
+
+lock = Lock()
+
+
+class AddressesRepository:
+    """Message repository"""
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(AddressesRepository, cls).__new__(cls)
+        return cls.instance
+
+    def __init__(self):
+        self.__addresses = {}
+
+    async def can_go(self, address) -> Tuple[bool, int]:
+        async with lock:
+            if address not in self.__addresses:
+                self.__addresses.update({address: datetime.now()})
+            seconds = (datetime.now() - self.__addresses[address]).seconds
+            if seconds > 60:
+                self.__addresses.update({address: datetime.now()})
+                return True, 0
+            else:
+                self.__addresses.update({address: self.__addresses[address] + timedelta(seconds=60 - seconds)})
+                return False, 60 - seconds
+
+
+addresses_repository = AddressesRepository()
