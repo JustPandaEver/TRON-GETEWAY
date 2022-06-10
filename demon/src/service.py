38b5c6fb707023a8df99d4355a2c7b563865d3f6
Mamedov Murad <mamedov_99b@mail.ru>
2022-06-10 14:19:45 +0300

    [ADD] send to balancer

--- demon/src/service.py
+++ demon/src/service.py
@@ -1,9 +1,13 @@
+import json
 import os
-from typing import List, Dict
+from typing import Optional, List, Dict
 
 import aiofiles
+import aio_pika
+from tronpy.tron import TAddress
 
-from config import NOT_SEND, logger
+from config import NOT_SEND
+from config import Config, logger
 
 
 async def send_all_from_folder_not_send():
@@ -24,10 +28,25 @@ async def send_all_from_folder_not_send():
 class Sender:
     @staticmethod
     async def send_to_balancer(message: List[Dict]):
-        pass
+        """Send transaction to Balancer"""
+        connection: Optional[aio_pika.RobustConnection] = None
+        channel: Optional[aio_pika.RobustChannel] = None
+        try:
+            connection = await aio_pika.connect_robust(url=Config.RABBITMQ_URL)
+            channel = await connection.channel()
+            await channel.declare_queue(Config.QUEUE_BALANCER)
+            await channel.default_exchange.publish(
+                message=aio_pika.Message(body=json.dumps(message).encode()),
+                routing_key=Config.QUEUE_BALANCER
+            )
+        except Exception as error:
+            logger.error(f"ERROR: {error}")
+        finally:
+            if connection is not None and not connection.is_closed:
+                await connection.close()
 
 
 class Getter:
     @staticmethod
-    async def get_addresses():
+    async def get_addresses() -> List[TAddress]:
         pass
