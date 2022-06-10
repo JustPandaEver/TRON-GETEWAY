7745f97442bf1bf051d8a49a09c775cc15337777
Mamedov Murad <mamedov_99b@mail.ru>
2022-06-10 15:49:12 +0300

    [FIX]

--- demon/config.py
+++ demon/config.py
@@ -26,9 +26,9 @@ class Config:
     NETWORK = os.getenv("NETWORK", "TESTNET").upper()
 
     RABBITMQ_URL = os.getenv("RABBITMQ_URL")
-    QUEUE_BALANCER = os.getenv("QUEUE_BALANCER")
+    QUEUE_BALANCER = os.getenv("QUEUE_BALANCER", "balancer_messanger")
 
-    NODE_URL = os.getenv("NODE_URL")
+    NODE_URL = os.getenv("NODE_URL", "http://tron-mainnet.mangobank.elcorp.io:8090")
     HELPER_KEYS = "8d375175-fa31-490d-a224-63a056adb60b"
 
     ADMIN_ADDRESS = os.getenv("ADMIN_ADDRESS")
