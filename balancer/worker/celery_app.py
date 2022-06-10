7d2e1d81d043020a3d46115a104bd4a4b7ccdb90
Mamedov Murad <mamedov_99b@mail.ru>
2022-06-10 17:17:14 +0300

    [ADD] celery app

---
+++ balancer/worker/celery_app.py
@@ -0,0 +1,14 @@
+from celery import Celery
+from config import Config
+
+
+celery_app = Celery(
+    "worker",
+    backend=Config.REDIS_URL,
+    broker=Config.RABBITMQ_URL
+)
+celery_app.conf.task_routes = {
+    "worker.celery_worker.send_transaction": "test-queue"
+}
+
+celery_app.conf.update(task_track_started=True)
