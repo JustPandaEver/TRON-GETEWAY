544337ae69d10ed4bb2011ad00084dc04c3ccbd3
Mamedov Murad <mamedov_99b@mail.ru>
2022-06-13 08:17:15 +0300

    [FIX] amount

--- demon/src/helper/utils.py
+++ demon/src/helper/utils.py
@@ -58,7 +58,7 @@ class Utils:
     def convert_data(data: str, decimals: int) -> Tuple[TAddress, decimal.Decimal]:
         return (
             Utils.to_base58check_address("41"+data[32:72]),
-            dml.create_decimal(int("0x"+data[72], 0) / decimals)
+            dml.create_decimal(int("0x"+data[72:], 0) / decimals)
         )
 
     @staticmethod
