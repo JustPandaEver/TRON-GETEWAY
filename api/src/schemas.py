caaf9b1f3a7836953daa4f42efa681065f7e5fe1
Mamedov Murad <mamedov_99b@mail.ru>
2022-06-20 08:43:05 +0300

    [FIX]

--- api/src/schemas.py
+++ api/src/schemas.py
@@ -7,13 +7,13 @@ from hdwallet.utils import generate_mnemonic
 class BodyCreateWallet(BaseModel):
     mnemonicPhrase: Optional[str] = Field(description="Mnemonic Phrase", default=None)
 
-    index: Optional[List[int, int]] = Field(
+    index: Optional[List[int]] = Field(
         description="Account and Address number. Only if mnemonicPhrase is available.", default=[0, 0]
     )
 
     def __init__(self, **kwargs):
-        super(BodyCreateWallet).__init__(**kwargs)
-        if self.mnemonicPhrase is None:
+        super(BodyCreateWallet, self).__init__(**kwargs)
+        if self.mnemonicPhrase is None or self.mnemonicPhrase == "string":
             self.mnemonicPhrase = generate_mnemonic(language="english", strength=128)
 
 
@@ -23,11 +23,11 @@ class ResponseCreateWallet(BaseModel):
     publicKey: str = Field(description="Public key from the wallet")
     address: str = Field(description="Wallet address")
 
-    index: Optional[List[int, int]] = Field(
+    index: Optional[List[int]] = Field(
         description="Account and Address number. Only if mnemonicPhrase is available.", default=[0, 0]
     )
 
     def __init__(self, **kwargs):
-        super(ResponseCreateWallet).__init__(**kwargs)
+        super(ResponseCreateWallet, self).__init__(**kwargs)
         if self.index is None or self.index == [0, 0]:
             del self.index
